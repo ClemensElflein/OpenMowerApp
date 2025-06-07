20307c52e5782798ac29baf2e3a6c4c5eef025bc
Vilius Kraujutis <viliusk@gmail.com>
2025-06-07 15:35:53 +0300

    build_and_deploy.sh: assume repos are cloned in same parent folder (#12)
    Assume all OM repos are cloned in the same parent folder for everyone
    
    It failed to build for me,
    because my repositories are cloned to different parent than `~/Dev`
    
    Co-authored-by: Vilius Kraujutis <vkraujutis@shift4.com>

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -4,6 +4,7 @@ rm -rf ./build
 flutter clean
 flutter build web --web-renderer canvaskit --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
 
-rm -rf ~/Dev/open_mower_ros/web
+# Replace built files in the `open_mower_ros` folder, which must be present in the same parent folder
+rm -rf ../open_mower_ros/web/
 
-cp -r ./build/web ~/Dev/open_mower_ros
+cp -r ./build/web ../open_mower_ros
