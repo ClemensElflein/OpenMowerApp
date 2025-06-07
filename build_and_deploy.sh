5ddd7cfe247bc6c512f829aab50463e91d6bd945
Vilius Kraujutis <viliusk@gmail.com>
2025-06-07 15:40:30 +0300

    build_and_deploy.sh: remove deprecated web-renderer parameter (#13)
    As mentioned in https://docs.flutter.dev/release/release-notes/release-notes-3.29.0
    the parameter is removed in latest flutter version:
    > [tool] Removes deprecated --web-renderer parameter. by @ditman in 159314
    
    Removing it from the script.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./build
 flutter clean
-flutter build web --web-renderer canvaskit --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
+flutter build web --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
 
 # Replace built files in the `open_mower_ros` folder, which must be present in the same parent folder
 rm -rf ../open_mower_ros/web/
