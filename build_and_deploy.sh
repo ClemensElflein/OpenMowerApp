811bd39adb1ad010b457fb91d9ac52e224d4cc93
Dmitry Dagunts <ddagunts@gmail.com>
2025-10-02 19:37:24 -0400

    Don't use externally hosted flutter resources

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./build
 flutter clean
-flutter build web --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
+flutter build web --no-web-resources-cdn --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
 
 # Replace built files in the `open_mower_ros` folder, which must be present in the same parent folder
 rm -rf ../open_mower_ros/web/
