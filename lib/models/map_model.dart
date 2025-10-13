5333e435b8d4943b0c096239c6768e06068fedea
Robert Vollmer <dev@robv.de>
2025-10-13 22:32:15 +0200

    Support for new map format

--- lib/models/map_model.dart
+++ lib/models/map_model.dart
@@ -1,14 +1,8 @@
 import 'dart:ui';
 
 class MapModel {
-    final List<MapAreaModel> navigationAreas = [];
-    final List<MapAreaModel> mowingAreas = [];
+    final List<Path> navigationAreas = [];
+    final List<Path> mowingAreas = [];
+    final List<Path> obstacles = [];
     double width = 0, height = 0, centerX = 0, centerY = 0, dockX = 0, dockY = 0, dockHeading = 0;
-}
-
-class MapAreaModel {
-  final Path outline;
-  final List<Path> obstacles;
-
-  MapAreaModel(this.outline, this.obstacles);
 }
